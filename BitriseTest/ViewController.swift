3d6e4b9ea8b86e0c80ab9e78f5342f903ea90e83
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-10-19 17:22:37 +0300

    First commit

---
+++ BitriseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseTest
+//
+//  Created by Gokhan Gultekin on 19.10.2018.
+//  Copyright © 2018 Gokhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
